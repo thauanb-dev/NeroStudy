f0f19856249b15fa7f5bb7a1a1f2fdb3e6addf9e
Thauan Barcellos <thauanb@gmail.com>
2026-06-27 12:04:13 -0300

    refactor: padroniza projeto em TypeScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import type {ReactNode} from 'react'
 import "../styles/global.css"
+
 
 export const metadata = {
     title:  "Nero Study",
     description: "Painel de estudos, foco e produtividade pessoal",
 }
 
-export default function RootLayout( { children }){
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout( { children }: RootLayoutProps){
     return(
         <html lang="pt-BR">
             <body>
